99a46f58d893d2feb4bde63550221373b66d95cf
PronetAI <pronetaicorp@gmail.com>
2022-07-12 15:25:42 -0700

    Update operators-in-typescript.ts

--- operators-in-typescript.ts
+++ operators-in-typescript.ts
@@ -1,4 +1,30 @@
 // Arthematic Operators 
-// +, - , *, \,  %, ++, --, **
+// +, - , *, /,  %, ++, --, **
 
-var var1
+var var1 : number = 10;
+var var2 : number = 20;
+var result : number = 0;
+
+result = a + b
+console.log(result);
+
+result = a - b
+console.log(result);
+
+result = a * b
+console.log(result);
+
+result = a / b
+console.log(result);
+
+result = a % b
+console.log(result);
+
+result = a ** b
+console.log(result);
+
+a++;
+console.log(result);
+
+b--;
+console.log(result);
